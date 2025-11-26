6823189ac2be80b05e76da160bace33f92d8d310
Axel84727 <135344010+Axel84727@users.noreply.github.com>
2025-11-26 18:08:56 -0300

    feat(physics): Implementación completa del Broad-Phase (Uniform Grid)
    - Implementadas las funciones world::limpieza() y world::repoblar() para gestionar la Rejilla Uniforme.
    - [Chore]: Se actualizó el .gitignore para excluir la carpeta .vscode/ y se eliminó su seguimiento.

--- src/world.cpp
+++ src/world.cpp
@@ -4,7 +4,18 @@
 // world(std::vector<body> &b, const vec2 &gravedad, float delta_time);
 world::world() : bodies(), gravedad(), delta_time(0.0f) {}
 
-world::world(const std::vector<body> &b_param, const vec2 &gravedad_param, float delta_time_param) : bodies(b_param), gravedad(gravedad_param), delta_time(delta_time_param) {}
+world::world(const std::vector<body> &b_param, const vec2 &gravedad_param, float delta_time_param) : bodies(b_param), gravedad(gravedad_param), delta_time(delta_time_param)
+{
+
+    float width = grid_info.max_x - grid_info.min_x;
+    float height = grid_info.max_y - grid_info.min_y;
+
+    int numCellsX = static_cast<int>(std::ceil(width / grid_info.cell_size));
+    int numCellsY = static_cast<int>(std::ceil(height / grid_info.cell_size));
+
+    int totalCells = numCellsX * numCellsY;
+    grid.resize(totalCells);
+}
 
 void world::update()
 {
@@ -21,5 +32,23 @@ void world::update()
 
         // 4. Limpieza
         b.aceleracion = vec2(0.0f, 0.0f);
+    }
+}
+void world::limpieza()
+{
+    for (auto &cell : grid)
+    {
+        cell.clear();
+    }
+}
+void world::repoblar()
+{
+    for (auto &body : bodies)
+    {
+        int tempIndex = get_grid_index(body.posicion);
+        if (tempIndex >= 0)
+        {
+            grid[tempIndex].push_back(&body);
+        }
     }
 }
